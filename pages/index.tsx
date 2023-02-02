9948707c925c967d5ddd54b36fc723a2b92f9600
Ifen Faridian <ifeniprun16@gmail.com>
2023-02-02 23:11:17 +0700

    add contact & scrollbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,13 @@ import About from '@/components/About'
 import WorkExperience from '@/components/WorkExperience'
 import Skills from '@/components/Skills'
 import Projects from '@/components/Projects'
+import ContactMe from '@/components/ContactMe'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
       <Head>
         <title>Jedd's Portfolio</title>
       </Head>
@@ -39,6 +40,9 @@ export default function Home() {
       </section>
 
       {/* Contact Me */}
+      <section id='contact' className='snap-start'>
+        <ContactMe />
+      </section>
     </div>
   )
 }
