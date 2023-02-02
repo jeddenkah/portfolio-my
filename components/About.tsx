707d530c93214c4a3584c6c511702d2fd8a6c0eb
Ifen Faridian <ifeniprun16@gmail.com>
2023-02-02 21:20:19 +0700

    add about & experience

--- components/About.tsx
+++ components/About.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 type Props = {}
 
 export default function About({ }: Props) {
 	return (
-		<div
+		<motion.div
+			initial={{
+				opacity: 0,
+			}}
+			whileInView={{
+				opacity: 1,
+			}}
+			transition={{
+				duration: 1.5,
+			}}
 			className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'
 		>
 			<h3
@@ -12,6 +22,31 @@ export default function About({ }: Props) {
 			>
 				About
 			</h3>
-		</div>
+			<motion.img
+				initial={{
+					x: -200,
+					opacity: 0,
+				}}
+				transition={{
+					duration: 1.2
+				}}
+				whileInView={{
+					x: 0,
+					opacity: 1,
+				}}
+				viewport={{
+					once: true,
+				}}
+				src='https://cdn.sanity.io/images/ltuexkre/production/ac8058b25cc880765f6549dd27223349f37a7c2f-1173x1458.jpg'
+				className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]'
+			/>
+
+			<div className='space-y-10 px-0 md:px-10'>
+				<h4 className='text-4xl font-semibold'>Here is a <span className='underline decoration-[#F7AB0A]/50'>little</span> background</h4>
+				<p className='text-base'>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, praesentium a, officia error, molestias quod tenetur facere exercitationem alias placeat odit! Quia quis vero impedit. Asperiores cumque accusamus esse perferendis itaque soluta repellendus temporibus architecto aut hic ipsum earum nam natus et culpa, dignissimos assumenda repellat eius sed a officiis atque? Ab molestiae ad aut sunt modi soluta nemo asperiores officiis, molestias facere rem cum temporibus possimus laboriosam amet! Nam.
+				</p>
+			</div>
+		</motion.div>
 	)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import { Inter } from '@next/font/google'
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import About from '@/components/About'
+import WorkExperience from '@/components/WorkExperience'
+import Skills from '@/components/Skills'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,19 +17,22 @@ export default function Home() {
 
       <Header />
 
-      {/* Hero */}
       <section id='hero' className='snap-center'>
         <Hero />
       </section>
 
-      {/* About */}
       <section id='about' className='snap-center'>
         <About />
       </section>
 
-      {/* Experience */}
+      <section id='experience' className='snap-center'>
+        <WorkExperience />
+      </section>
 
       {/* Skills */}
+      <section id='skills' className='snap-start'>
+        <Skills />
+      </section>
 
       {/* Projects */}
 
